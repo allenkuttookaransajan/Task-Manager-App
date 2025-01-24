a047fdd9cb61340371b3b9ad7e4ec9387a7c5e65
allenkuttookaransajan <59818266+allenkuttookaransajan@users.noreply.github.com>
2025-01-24 21:05:10 +0530

    Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/features/auth/pages/signup_page.dart'; // Importing the Flutter material package
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp()); // Main function that starts the Flutter application
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Task App',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Task App', // Title of the application
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Default theme color
       ),
+      home: const SignupPage(),
     );
   }
 }
