abcbf1a58ba6890b96838948fe57b4148be8e3fe
allenkuttookaransajan <59818266+allenkuttookaransajan@users.noreply.github.com>
2025-01-30 10:53:06 +0530

    drizzle-orm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!!!")
+import express from "express";
+import authRouter from "./routes/auth";
+
+const app = express();
+
+app.use(express.json());
+app.use("/auth",authRouter)
+
+app.get("/",(req,res) => {
+    res.send("Welcome to my app!!!");
+});
+
+app.listen(8000, () => {
+    console.log("Server Started on port 8000");
+});
